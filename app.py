ac8b48d244f013cd839eb8cb8e27e385edfc5f3b
Joey Love <joeylove26@gmail.com>
2024-01-02 22:48:58 -0700

    simplified/traditional and mandarin now appear based on the checkboxes

--- app.py
+++ app.py
@@ -25,10 +25,7 @@ def index():
 
 @app.route('/process_click_on_character', methods=['POST'])
 def process_click_on_character():
-    character = request.get_json()['character']
-    print(request.get_json())
-    # Replace with your actual Python function
-    result = create_character_info_sheet(character)
+    result = create_character_info_sheet(request.get_json())
     return jsonify(result=result)
 
 
@@ -57,18 +54,27 @@ def load_input_strings_from_file():
 
 
 
-def create_character_info_sheet(character):
+def create_character_info_sheet(json_data):
 
-    return_str = ''
+    character = json_data['character']
     character_entry = d.lookup(character)
-    return_str += character_entry.simp + " | " + character_entry.trad + '<br><br>'
+    #TODO: fix some traditional characters not appearing in the lookup
 
-    for dict_entry in character_entry.definition_entries:
-        return_str += f'<span style="color:{tone_color_dict[dict_entry.pinyin[-1]]}; font-size: 30px "> • {to_tone(dict_entry.pinyin)} </span>'
+    # a string that accumulates html elements based on which language options are enabled
+    return_str = ''
+    
+    # Adds an element for the chinese simplified and traditional variants
+    if json_data['chineseSimpTradCheckbox']:
+        return_str += character_entry.simp + " | " + character_entry.trad + '<br><br>'
 
-        for definition in dict_entry.definitions:
-            return_str += f' - {definition} <br>'
-        return_str += '<br>'
+    # Adds an element for mandarin defintions and readings
+    if json_data['chineseMandarinCheckbox']:
+        for dict_entry in character_entry.definition_entries:
+            return_str += f'<span style="color:{tone_color_dict[dict_entry.pinyin[-1]]}; font-size: 30px "> • {to_tone(dict_entry.pinyin)} </span>'
+
+            for definition in dict_entry.definitions:
+                return_str += f' - {definition} <br>'
+            return_str += '<br>'
 
     return return_str
 
