a3af240fb457b12473a07c825e9c411d1104090e
Joey Love <joeylove26@gmail.com>
2024-01-27 05:47:09 -0700

    made language checkboxes checked by default

--- static/script.js
+++ static/script.js
@@ -107,6 +107,7 @@ function fetchSearchResults(searchString, searchType) {
         if (xhr.status === 200) {
             // Parse the JSON response from the server
             var response = JSON.parse(xhr.responseText);
+            console.log(response);
             var characterSet = response.inputString;
             generateCharacterElements(characterSet);
         }
